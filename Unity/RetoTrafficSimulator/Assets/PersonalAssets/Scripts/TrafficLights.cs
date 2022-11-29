f529851cb66efba594bb6760fd6d534fc0c070ac
Sergio Carrera <75439050+Chimeroni@users.noreply.github.com>
2022-11-29 17:51:17 -0600

    Semaforos Funcionales, Modelo ideal

--- Unity/RetoTrafficSimulator/Assets/PersonalAssets/Scripts/TrafficLights.cs
+++ Unity/RetoTrafficSimulator/Assets/PersonalAssets/Scripts/TrafficLights.cs
@@ -4,83 +4,48 @@ using UnityEngine;
 
 public class TrafficLights : MonoBehaviour
 { 
-    public GameObject light;
-    public float s_time; //  The Starting time needs to vary from 1 to 2 seconds for each Square
+    public GameObject red_light;
+    public GameObject yellow_light;
+    public GameObject green_light;
 
-    private Vector3 red_y;
-    private Vector3 yellow_y;
-    private Vector3 green_y;
+    public float s_time; //  The Starting time needs to vary from 1 to 2 seconds for each Square
 
     private bool greenLight = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        red_y = light.transform.position;
-        yellow_y = red_y - new Vector3(0, -1, 0);
-        green_y = red_y - new Vector3(0, -2, 0);
+        red_light.SetActive(true);
+        yellow_light.SetActive(false);
+        green_light.SetActive(false);
 
-    // InvokeRepeating(StartCoroutine(ChangeLights()), startingTime, 5f);
-    StartCoroutine(TheRoutine(s_time));
+
+        // InvokeRepeating(StartCoroutine(ChangeLights()), startingTime, 5f);
+        StartCoroutine(ChangeLights(s_time));
     }
 
-    IEnumerator TheRoutine(float time)
+    IEnumerator ChangeLights(float time)
     {
         yield return new WaitForSeconds(time);
         
         while (true)
         {
-            StartCoroutine(ChangeLights());
+            // Green Light
+            red_light.SetActive(false);
+            green_light.SetActive(true);
             yield return new WaitForSeconds(5f);
-        }
-     }
 
-    IEnumerator ChangeLights()
-    {
-        if (greenLight == true)
-        {
+            // Yellow Light
+            green_light.SetActive(false);
             for (int i = 0; i < 6; i++)
             {
-                if (i%2 == 0)
-                {
-                    light.transform.position = new Vector3(0, -100, 0);
-                }
-                else
-                {
-                    TurnOn("YellowLight");
-                }
+                if (i % 2 == 0) { yellow_light.SetActive(false); } else { yellow_light.SetActive(true); }
                 yield return new WaitForSeconds(0.5f);
             }
-            greenLight = false;
-            TurnOn("RedLight");
+            // Red Light
+            yellow_light.SetActive(false);
+            red_light.SetActive(true);
+            yield return new WaitForSeconds(8f);
         }
-        else if (greenLight == false)
-        {
-            TurnOn("GreenLight");
-        }
-    }
-
-   
-
-    void TurnOn(string light_seq)
-    {
-        Debug.Log("Turning " + light_seq + " on...");
-
-        if (light_seq == "YellowLight")
-        {
-            light.transform.position = yellow_y;
-            light.GetComponent<Light>().color = Color.yellow;
-        }
-        else if (light_seq == "RedLight")
-        {
-            light.transform.position = red_y;
-            light.GetComponent<Light>().color = Color.red;
-        }
-        else if (light_seq == "GreenLight")
-        {
-            light.transform.position = green_y;
-            light.GetComponent<Light>().color = new Color32(61, 161, 27, 255);
-        }
-    }
-    
+     }
 }
